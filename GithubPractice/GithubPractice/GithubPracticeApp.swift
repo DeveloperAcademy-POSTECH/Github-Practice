a27a08fd82878cc575e6eea30522bcd1634aaa7e
YeongJin Jeong <yungzin98@naver.com>
2025-05-22 14:30:14 +0900

    [chore] initialize project

---
+++ GithubPractice/GithubPractice/GithubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubPracticeApp.swift
+//  GithubPractice
+//
+//  Created by 정영진 on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
