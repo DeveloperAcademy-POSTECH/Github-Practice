194c9c57d0c825478fe621ba203f6a14e57ad4c9
hyejoon-jooni <whgpwns0528@naver.com>
2025-05-23 17:16:14 +0900

    [Feat]JooniView제작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/JooniView.swift
@@ -0,0 +1,26 @@
+//
+//  JooniView.swift
+//  GithubPractice
+//
+//  Created by 조혜준 on 5/23/25.
+//
+
+import SwiftUI
+
+struct JooniView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Jooni"
+    
+    var team: String = "벌써데이"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    JooniView()
+}
