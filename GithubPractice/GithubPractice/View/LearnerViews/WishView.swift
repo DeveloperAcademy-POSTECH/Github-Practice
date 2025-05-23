7ef08df791ed5a69d2bfdd5edb700477ed0ec11d
wish627 <wishful.to@gmail.com>
2025-05-23 12:18:07 +0900

    [Feat] WishView 수정

--- GithubPractice/GithubPractice/View/LearnerViews/WishView.swift
+++ GithubPractice/GithubPractice/View/LearnerViews/WishView.swift
@@ -13,10 +13,6 @@ struct WishView: LearnerView {
     var team: String = "경조사🌼실🌺무🌹팀🌸"
     
     var body: some View {
-        ZStack {
-            background(Color.red)
-        }
-        
         Text("저의 이름은 \(name)입니다.")
             .font(.largeTitle)
         Text("저의 팀은 \(team) 입니다.")
