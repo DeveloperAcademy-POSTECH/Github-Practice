1045ff0c6e5c90a4ea076ecbc9b062d322c1e919
averysjung <averyseojinjung@gmail.com>
2025-05-23 12:16:20 +0900

    [Feat]AveryView추가

---
+++ GithubPractice/GithubPractice/View/LearnerViews/AveryView.swift
@@ -0,0 +1,26 @@
+//
+//  AveryView.swift
+//  GithubPractice
+//
+//  Created by Seojin Jung on 5/23/25.
+//
+
+import SwiftUI
+
+struct AveryView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Avery"
+    
+    var team: String = "Like Ginie"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    AveryView()
+}
